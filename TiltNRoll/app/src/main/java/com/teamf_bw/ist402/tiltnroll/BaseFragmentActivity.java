a568cb57742229f9ff80bd27f92c0f525a25193a
Jesse Jurden <jnunk.cuber@gmail.com>
2015-04-29 19:09:38 -0700

    Added some more files
    stuff

---
+++ TiltNRoll/app/src/main/java/com/teamf_bw/ist402/tiltnroll/BaseFragmentActivity.java
@@ -0,0 +1,24 @@
+package com.teamf_bw.ist402.tiltnroll;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Jesse on 4/29/2015.
+ */
+public class BaseFragmentActivity extends FragmentActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_crime);
+//
+//        FragmentManager fm = getSupportFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+//
+//        if (fragment == null) {
+//            fragment = new CrimeFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragmentContainer, fragment)   // "Meat"
+//                    .commit();
+//        }
+//    }
+}
--- TiltNRoll/app/src/main/java/com/teamf_bw/ist402/tiltnroll/GameActivity.java
+++ TiltNRoll/app/src/main/java/com/teamf_bw/ist402/tiltnroll/GameActivity.java
@@ -1,13 +1,13 @@
 package com.teamf_bw.ist402.tiltnroll;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-
-public class GameActivity extends ActionBarActivity {
+public class GameActivity extends Activity {
 
     private GameSurfaceView gameView;
 
